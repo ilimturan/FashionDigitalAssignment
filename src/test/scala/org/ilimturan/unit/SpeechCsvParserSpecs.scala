84666c2a38b5b328ee3fd3f6a40b5fd8be78f3f6
ilimturan <ilimturan@hotmail.com>
2023-11-30 23:24:09 +0300

    Add SpeechCsvParserSpecs' unit test

---
+++ src/test/scala/org/ilimturan/unit/SpeechCsvParserSpecs.scala
@@ -0,0 +1,36 @@
+package org.ilimturan.unit
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.stream.scaladsl.Sink
+import org.apache.commons.io.input.BOMInputStream
+import org.ilimturan.parser.SpeechCsvParser
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.{Matchers, WordSpec}
+
+class SpeechCsvParserSpecs
+    extends WordSpec
+    with Matchers
+    with ScalatestRouteTest
+    with ScalaFutures {
+
+  // for futureValue timeout
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(3, Seconds)))
+
+  "run SpeechCsvParser tests" should {
+
+    val parser = new SpeechCsvParser()
+
+    "when parse file inputstream" in {
+
+      val inputStream    = getClass.getResourceAsStream("/politics.csv")
+      val bomInputStream = new BOMInputStream(inputStream)
+
+      val source = parser.toAkkaSource(bomInputStream)
+      val result = source.runWith(Sink.seq).map(_.size)
+
+      result.futureValue shouldEqual 4
+    }
+
+  }
+}
